0136e78b63850a3e61045919874d79dc8ccba8e6
tjvans <14749212+tjvans@users.noreply.github.com>
2023-10-19 12:07:29 +0200

    style: wrap avatar with card

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,17 @@
 import Avatar from "./Avatar";
 
 export default function Profile(props) {
-  return <Avatar {...props} />
+  return (
+    <Card>
+      <Avatar {...props} />
+    </Card>
+  )
+}
+
+function Card({children}) {
+  return (
+    <div className="card">
+      {children}
+    </div>
+  )
 }
