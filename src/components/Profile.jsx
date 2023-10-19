b42472e67187fa566786d9dd5e7d83bc0f1ff8bd
tjvans <14749212+tjvans@users.noreply.github.com>
2023-10-19 11:53:00 +0200

    fix: merge conflict

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,5 @@
 import Avatar from "./Avatar";
 
-<<<<<<< HEAD
 export default function Profile(props) {
   return <Avatar {...props} />
-=======
-export default function Profile() {
-  return <Avatar />;
->>>>>>> refs/remotes/origin/main
 }
