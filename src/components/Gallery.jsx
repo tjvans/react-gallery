5bc6df5e001ad08bbd279ea485f58b8f80673860
tjvans <tjvans@users.noreply.github.com>
2023-10-20 10:33:18 +0000

    Apply formating changes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,9 +2,9 @@ import Profile from "./Profile";
 import { people } from "../assets/data";
 
 export default function Gallery() {
-  const listItems = people.map(person =>
-      <Profile key={person.name} size={90} person={person} />
-    )
+  const listItems = people.map((person) => (
+    <Profile key={person.name} size={90} person={person} />
+  ));
   return (
     <section>
       <h1>Amazing scientists</h1>
