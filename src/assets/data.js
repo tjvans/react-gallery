288e36bb6152294df55032f1fe1967d0f5e2d7ec
tjvans <14749212+tjvans@users.noreply.github.com>
2023-10-20 10:56:54 +0200

    move people array to separate file

---
+++ src/assets/data.js
@@ -0,0 +1,29 @@
+export const people = [
+    {
+      name: "Gregorio Y. Zara",
+      image: {
+        baseUrl: "https://i.imgur.com/",
+        imageId: "7vQD0fP",
+        imageType: ".jpg",
+      },
+      profession: "Scientist",
+    },
+    {
+      name: "Katherine Johnson",
+      image: {
+        baseUrl: "https://i.imgur.com/",
+        imageId: "MK3eW3A",
+        imageType: ".jpg",
+      },
+      profession: "Mathematician",
+    },
+    {
+      name: "Lin Lanying",
+      image: {
+        baseUrl: "https://i.imgur.com/",
+        imageId: "1bX5QH6",
+        imageType: ".jpg",
+      },
+      profession: "Physicist",
+    },
+  ];
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,36 +1,6 @@
 import Profile from "./Profile";
 
 export default function Gallery() {
-  const people = [
-    {
-      name: "Gregorio Y. Zara",
-      image: {
-        baseUrl: "https://i.imgur.com/",
-        imageId: "7vQD0fP",
-        imageType: ".jpg",
-      },
-      profession: "Scientist",
-    },
-    {
-      name: "Katherine Johnson",
-      image: {
-        baseUrl: "https://i.imgur.com/",
-        imageId: "MK3eW3A",
-        imageType: ".jpg",
-      },
-      profession: "Mathematician",
-    },
-    {
-      name: "Lin Lanying",
-      image: {
-        baseUrl: "https://i.imgur.com/",
-        imageId: "1bX5QH6",
-        imageType: ".jpg",
-      },
-      profession: "Physicist",
-    },
-  ];
-
   let person = {
     name: "Gregorio Y. Zara",
     image: {
