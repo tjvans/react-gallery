09e9ceb998f45de60355ddf4e27314d02da807c8
tjvans <tjvans@users.noreply.github.com>
2023-10-24 11:34:13 +0000

    Apply formating changes

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,10 +1,10 @@
 export default function Bio({ person }) {
-    return (
-        <div>
-            <h1>Name</h1>
-            <p>{person.name}</p>
-            <h1>Profession</h1>
-            <p>{person.profession}</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Name</h1>
+      <p>{person.name}</p>
+      <h1>Profession</h1>
+      <p>{person.profession}</p>
+    </div>
+  );
 }
