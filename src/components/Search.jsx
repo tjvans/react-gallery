37d60c055fc1f19bb702ec62c2801c5d55f663b3
tjvans <14749212+tjvans@users.noreply.github.com>
2023-10-26 15:12:14 +0200

    update search styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ export default function Search() {
   return (
     <div className='search-container'>
       <h1>Academic gallery</h1>
-      <label >
+      <label className='search-input'>
         <h3>Search: </h3>
         <input type="text" />
       </label>
