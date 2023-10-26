979b34a36e95da18f40c750832f1e1c3e306ae2b
tjvans <14749212+tjvans@users.noreply.github.com>
2023-10-26 13:37:23 +0200

    create search component input field

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,10 @@ export default function Search() {
   return (
     <div className='search-container'>
       <h1>Academic gallery</h1>
+      <label >
+        <h3>Search: </h3>
+        <input type="text" />
+      </label>
     </div>
   )
 }
