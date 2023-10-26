5ea0d1301ba8eae76bd47996e22bd3acae5ca65f
tjvans <14749212+tjvans@users.noreply.github.com>
2023-10-26 13:51:32 +0200

    create search state variable

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,10 @@
 import Profile from "./Profile";
 import Search from "./Search";
 import { people } from "../assets/data";
+import { useState } from "react";
 
 export default function Gallery() {
+  const [query, setQuery] = useState('')
   
   const listItems = people.map((person) => (
     <Profile key={person.name} size={90} person={person} />
