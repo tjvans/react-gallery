23735b87f1807e7202f46dea02084ed488e9ad61
tjvans <tjvans@users.noreply.github.com>
2023-10-31 09:16:54 +0000

    Apply formating changes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -7,17 +7,22 @@ export default function Gallery() {
   const [query, setQuery] = useState("");
 
   function handleChange(e) {
-    setQuery(e.target.value)
+    setQuery(e.target.value);
   }
 
   function queryFilter(items, query) {
-    query = query.toLowerCase()
-    const person = items.filter(item => {
-      return item.name.split(' ').some(word => 
-        word.toLowerCase().startsWith(query)) || 
-        item.profession.split(' ').some(word => 
-          word.toLowerCase().startsWith(query))})
-    return person
+    query = query.toLowerCase();
+    const person = items.filter((item) => {
+      return (
+        item.name
+          .split(" ")
+          .some((word) => word.toLowerCase().startsWith(query)) ||
+        item.profession
+          .split(" ")
+          .some((word) => word.toLowerCase().startsWith(query))
+      );
+    });
+    return person;
   }
 
   const listItems = queryFilter(people, query).map((person) => (
