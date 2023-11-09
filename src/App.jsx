fca8e5e42fa17301c043b78371cb566a2b19042c
tjvans <14749212+tjvans@users.noreply.github.com>
2023-11-09 15:24:13 +0200

    fix: correct github logo path

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import "./App.css";
 import Gallery from "./components/Gallery";
+import "./App.css";
 
 function App() {
   return (
     <>
       <a id="github-link" href="https://github.com/tjvans/react-gallery">
         <span>Project files on </span>
-        <img src="../public/github-mark.svg" alt="The GitHub logo"/>
+        <img src='./github-mark.svg' alt="The GitHub logo"/>
       </a>
       <Gallery />
     </>
