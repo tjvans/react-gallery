62353294c2fb6086473f543b0de2d3c50eab27fd
tjvans <14749212+tjvans@users.noreply.github.com>
2023-11-09 13:44:08 +0200

    update styling and add github link

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Gallery from "./components/Gallery";
 function App() {
   return (
     <>
+      <a id="github-link" href="https://github.com/tjvans/react-gallery">
+        <span>Project files on </span>
+        <img src="../public/github-mark.svg" alt="The GitHub logo"/>
+      </a>
       <Gallery />
     </>
   );
