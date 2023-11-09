30675e805c63b0a9728b39f2c3c1e662ca4a5051
tjvans <tjvans@users.noreply.github.com>
2023-11-09 13:27:30 +0000

    Apply formating changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <a id="github-link" href="https://github.com/tjvans/react-gallery">
         <span>Project files on </span>
-        <img src='./github-mark.svg' alt="The GitHub logo"/>
+        <img src="./github-mark.svg" alt="The GitHub logo" />
       </a>
       <Gallery />
     </>
